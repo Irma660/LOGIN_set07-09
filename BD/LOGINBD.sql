fa07a8f4444b999cd0ccf3357f1d3442d720161e
Irma660 <1325455@senati.pe>
2023-09-07 12:38:53 -0500

    agregando contador

--- BD/LOGINBD.sql
+++ BD/LOGINBD.sql
@@ -4,10 +4,10 @@ go
  go 
  create table USUARIO
  (
- id_usuario varchar(4) primary key,
- nombre varchar,
- usuario varchar,
- contraseña varchar (8));
+ id_usuario int primary key identity(1,1),
+ nombre varchar not null,
+ usuario varchar not null,
+ contraseña varchar (8) not null);
  insert into USUARIO values('1111','Paola Sanchez','Pao123','ert509ul')
 
  insert into USUARIO (nombre, usuario, contraseña)
