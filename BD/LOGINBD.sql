dfd6ee91e84f852026582a549f35fbcba93efc58
Irma660 <1325455@senati.pe>
2023-09-07 12:48:38 -0500

    modificando errores

--- BD/LOGINBD.sql
+++ BD/LOGINBD.sql
@@ -8,7 +8,8 @@ go
  nombre varchar not null,
  usuario varchar not null,
  contraseña varchar (8) not null);
- insert into USUARIO values('1111','Paola Sanchez','Pao123','ert509ul')
+
 
  insert into USUARIO (nombre, usuario, contraseña)
- values('nombreU','usuarioE','contraseñaE')
+ values('Paola Sanchez','Pao123','ert509ul')
+ 
