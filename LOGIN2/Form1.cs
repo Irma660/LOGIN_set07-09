8c2a316a1770da9b1cc32b19340e54678b6e84d9
Irma660 <1325455@senati.pe>
2023-09-07 14:43:36 -0500

    realizando conexión con BD

--- LOGIN2/Form1.cs
+++ LOGIN2/Form1.cs
@@ -12,9 +12,26 @@ namespace LOGIN2
 {
     public partial class Form1 : Form
     {
+        private string conexion = "Data Source=DESKTOP-7LDGQBD;Initial Catalog = LOGINBD; Integrated Security = True";
         public Form1()
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
